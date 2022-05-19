2910929793c778958b32601c2391ec878c94d305
Cristian <cdavidq.22@gmail.com>
2022-05-18 19:12:12 -0500

    Tarea 2: Hola mundo

---
+++ src/main/java/ec/edu/uce/Tarea2RepoCq1Application.java
@@ -0,0 +1,22 @@
+package ec.edu.uce;
+
+import java.util.logging.Logger;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2RepoCq1Application {
+
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2RepoCq1Application.class, args);
+		
+		System.out.println("HOLA MUNDO");
+		
+	}
+
+}
